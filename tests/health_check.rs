9e4d904405b33c3a08b5f2b969aefd59c50d42bb
Chaitanya <chaitanyam187@gmail.com>
2024-10-27 01:15:36 +0530

    Setup send_email Method and Update Unit Test For The Same Method Using Mock Server

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -44,7 +44,11 @@ async fn spawn_test_server() -> TestApp {
         .email_client
         .sender()
         .expect("Invalid sender email address");
-    let email_client = EmailClient::new(configuration.email_client.base_url, sender_email);
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+    );
 
     let pool = configure_database(&configuration.database).await;
 
