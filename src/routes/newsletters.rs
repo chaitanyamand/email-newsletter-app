d0d127ffc6974497c4b4bc2c0fda91641f626e43
Chaitanya <chaitanyam187@gmail.com>
2024-11-30 21:11:44 +0530

    Store Hashed Passwords In DB

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -8,6 +8,7 @@ use actix_web::{HttpRequest, ResponseError};
 use anyhow::Context;
 use reqwest::header::{self, HeaderValue};
 use secrecy::{ExposeSecret, Secret};
+use sha3::Digest;
 use sqlx::PgPool;
 
 #[derive(thiserror::Error)]
@@ -86,10 +87,12 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
+    let password_hash = sha3::Sha3_256::digest(credentials.password.expose_secret().as_bytes());
+    let password_hash = format!("{:x}", password_hash);
     let user_id = sqlx::query!(
-        r#"SELECT user_id FROM users WHERE username = $1 AND password = $2"#,
+        r#"SELECT user_id FROM users WHERE username = $1 AND password_hash = $2"#,
         credentials.username,
-        credentials.password.expose_secret()
+        password_hash
     )
     .fetch_optional(pool)
     .await
